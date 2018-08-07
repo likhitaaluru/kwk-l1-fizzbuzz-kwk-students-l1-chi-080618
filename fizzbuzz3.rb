ae73c90ef62280cac727cbaf89fb3f648ad4edf5
likhitaaluru <likhitaaluru@gmail.com>
2018-08-07 19:14:40 +0000

    automatically backed up by learn

--- fizzbuzz3.rb
+++ fizzbuzz3.rb
@@ -1,7 +1,9 @@
 def fizzbuzz (number)
  if 3*(number/3)==number && 5*(number/5)==number
   "FizzBuzz"
- elsif 5*(number/5)==number
+end
+
+ if 5*(number/5)==number
  "Buzz"
  elsif 3*(number/3)==number
  "Fizz"
