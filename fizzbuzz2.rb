f64d46aa29a539ced2961b46e2186ef41b53c188
likhitaaluru <likhitaaluru@gmail.com>
2018-08-07 19:06:13 +0000

    automatically backed up by learn

--- fizzbuzz2.rb
+++ fizzbuzz2.rb
@@ -1,1 +1,13 @@
-5*(number/5)==number
+
+
+def fizzbuzz (number)
+ if number%3==0 && number%5==0
+  "FizzBuzz"
+ elsif 5*(number/5)==number
+ "Buzz"
+ elsif number%3==0
+ "Fizz"
+ else
+  nil 
+ end
+end
